0683de79aa64fff7dab5a832df5ac3b32e4fc378
M. Oly Mahmud <mugdhodzs38@gmail.com>
2023-03-28 01:47:12 +0600

    Add files via upload

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const users = require('./api/users');
+const moment = require('moment');
+
 
 /* app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname,'public/index.html'));
 }); */
 
+const logger = (req, res, next) => {
+    console.log(`${req.protocol}://${req.hostname}:500${req.url}  ${moment().format('MMMM Do YYYY, h:mm:ss a')}`);
+    next();
+}
+app.use(logger);
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/api/users', (req, res) => {
@@ -26,12 +33,12 @@ app.post('/api/users', (req, res) => {
 app.put('/api/users', (req, res) => {
     users.map(
         (user) => {
-            if(user.id === 11){
+            if (user.id === 11) {
                 user.friend = 'Mugdho Mahmud';
             }
         }
     );
-    res.json({message : "Updated", users});
+    res.json({ message: "Updated", users });
 
 })
 
